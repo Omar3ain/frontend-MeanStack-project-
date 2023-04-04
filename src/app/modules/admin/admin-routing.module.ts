cbf4be45a14172b1973ca85fc43e9b2ce14f19f4
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-04-04 19:57:26 +0200

    add compnent compnents directory in admin & add category directory contains voiew and create compnents

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent as ListCategories } from './components/categories/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories/list', component: ListCategories},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
