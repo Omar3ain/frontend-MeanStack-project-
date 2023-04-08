d873908f1bfd45be09e3289c61a516b32b9f7f16
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-04-08 13:40:35 +0200

    make top five categories

--- src/app/modules/library/libModules/sections/categories/categories.component.ts
+++ src/app/modules/library/libModules/sections/categories/categories.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import AOS from 'aos';
+import { CategoryService } from '../../../services/category.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
@@ -8,6 +10,18 @@ import AOS from 'aos';
 })
 export class CategoriesComponent {
   categoryImagePath = 'assets/library/header-image.jpg';
+  categories: any[] = [];
+  constructor (private CategoryService: CategoryService, private router: Router ) {
+    this.CategoryService.getTopFive().subscribe(categories => {
+      this.categories = categories.categories;
+    })
+  }
+
+  openCategory(id: string) {
+    console.log(id)
+    this.router.navigate(['categories',id]);
+  }
+
   ngOnInit() {
     AOS.init();
   }
