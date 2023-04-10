2212b86bfe1fe76d222763b8d7b282a8dc5deff1
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-04-10 19:10:08 +0200

    add reviews to book

--- src/app/modules/library/libModules/book/book.module.ts
+++ src/app/modules/library/libModules/book/book.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from 'src/app/sharedComponents/search-bar/search-bar.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { BookRoutingModule } from './book-routing.module';
 import { BooksComponent  } from './components/books/books.component';
@@ -12,7 +13,8 @@ import { BookComponent } from './components/book/book.component';
     SearchBarComponent],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    NgbModule
   ]
 })
 export class BookModule { }
