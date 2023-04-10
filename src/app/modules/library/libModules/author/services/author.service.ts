6fe8ddfd5d66e78e1550994d3dec6142f7c44978
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-04-11 00:47:04 +0200

    author detailes

--- src/app/modules/library/libModules/author/services/author.service.ts
+++ src/app/modules/library/libModules/author/services/author.service.ts
@@ -1,27 +1,58 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { author } from '../components/list-authors/list-authors.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
-  static getAuthor(authorId: any) {
-    throw new Error('Method not implemented.');
-  }
 
-  domin : string = "http://localhost:3000"
-  authorsUrl = `${this.domin}/authors`;
-  
+  currentPage = 1;
+  authorsPerPage = 3;
+  authors: author[] = [];
+  authorUrl = 'http://localhost:3000/authors';
+  bookUrl = 'http://localhost:3000/books'
+
   constructor(private http: HttpClient) { }
+
+  getAuthors(): Observable<author[]> {
+    return this.http.get<author[]>(this.authorUrl).pipe(
+      map((authors: author[]) => {
+        this.authors = authors;
+        return authors;
+      })
+    );
+  }
   handleError(error: HttpErrorResponse) {
     return throwError(() => error);
   }
+  getAuthorsById(id: string) {
+    return this.http.get(`${this.authorUrl}/authors/${id}`).pipe(catchError((this.handleError)));
 
-  getAuthors(): Observable<any> {
-    return this.http.get(this.authorsUrl).pipe(catchError((this.handleError)));
   }
-  getAuthor(id : string) : Observable<any>  {
-    return this.http.get(`${this.authorsUrl}/${id}`).pipe(catchError((this.handleError)));
+
+  getAuthorsForPage(): author[] {
+    const startIndex = (this.currentPage - 1) * this.authorsPerPage;
+    const endIndex = startIndex + this.authorsPerPage;
+    return this.authors.slice(startIndex, endIndex);
   }
+
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages()) {
+      this.currentPage++;
+    }
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.authors.length / this.authorsPerPage);
+  }
+
 }
