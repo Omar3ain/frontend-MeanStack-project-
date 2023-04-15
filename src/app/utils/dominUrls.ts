3efd9ce9674fd0b2a510f901758652cc4b4dfac8
omar3ain <omar3ain@gmail.com>
2023-04-15 19:32:22 +0200

    domain centrized

---
+++ src/app/utils/dominUrls.ts
@@ -0,0 +1,4 @@
+const devDomain : string = "http://localhost:3000";
+const prodDomain : string = "https://tsbookvalley.onrender.com";
+const currentDomain : string = prodDomain;
+export default currentDomain;
